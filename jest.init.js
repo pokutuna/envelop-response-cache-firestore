1a2378fecf1a516ff20434c88d8939ca45e9090d
pokutuna <mail@pokutuna.com>
2022-09-12 10:14:20 +0900

    doc: update comment

--- jest.init.js
+++ jest.init.js
@@ -1,3 +1,3 @@
-// force use emulator to prevent accidental operate remote
+// force use emulator to prevent accidental operate remote DB
 process.env.FIRESTORE_EMULATOR_HOST =
   process.env.FIRESTORE_EMULATOR_HOST ?? 'localhost:8500';
